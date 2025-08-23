b6032b2c45a93e21a9be07aa71856642d4276b8c
anil <anil12345@gmail.com>
2025-08-23 13:02:50 +0530

    arthematic.sh file is comitted successfully

---
+++ arthematic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+name=sudarshan
+echo "Hello!,$name"
+echo " "
+num1=100
+num2=200
+sum=$((num1+num2))
+echo "The addition of these numbers are: $sum"
+multi=$((num1*num2))
+echo "The multiplication of these numbers are: $multi"
+sub=$((num2-num1))
+echo "The subtraction of these numbers are: $sub"
+dev=$((num2/num1))
+echo "The division of these numbers are: $dev"
+rem=$((num2%num1))
+echo "The remainder of these numbers are: $rem"
